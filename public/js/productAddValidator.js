6c5677432c05ca6f3cab3e45d5deaed6366e33d3
facundo ramirez <ramirezfacundo1996@gmail.com>
2024-04-11 00:59:55 -0300

    [Validations]_validaciones funcional

--- public/js/productAddValidator.js
+++ public/js/productAddValidator.js
@@ -1,175 +1,22 @@
-console.log("register validator success!");
+console.log("product validator success!");
 
 const $ = (id) => document.getElementById(id);
+const omitFields = [14, 13, 5];
+$("");
 
-//precio
-$("price").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-price").innerHTML = "La cantidad es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-price").innerHTML = null;
-      break;
-  }
-});
+$("form-add-product").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-//nombre
-$("name").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-name").innerHTML = "La cantidad es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-name").innerHTML = null;
-      break;
-  }
-});
+  console.log(this.elements);
 
-//cantidad
-$("quantityInStock").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-quantityInStock").innerHTML = "La cantidad es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-quantityInStock").innerHTML = null;
-      break;
-  }
-});
+  let error = false;
 
-//marca
-$("brand").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-brand").innerHTML = "La marca es obligatoria";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-brand").innerHTML = null;
-      break;
+  for (let i = 0; i < this.elements.length - 2; i++) {
+    if (!this.elements[i].value && !omitFields.includes(i)) {
+      error = true;
+      this.elements[i].classList.add("is-invalid");
+    }
   }
-});
 
-//tamaño
-$("tamanio").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-tamanio").innerHTML = "El tamaño es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-tamanio").innerHTML = null;
-      break;
-  }
-});
-
-//color
-$("color").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-color").innerHTML = "El color es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-color").innerHTML = null;
-      break;
-  }
-});
-
-//descuento
-$("discount").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-discount").innerHTML = "El tamaño es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-discount").innerHTML = null;
-      break;
-  }
-});
-
-//descripcion
-$("description").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-description").innerHTML = "La description es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-description").innerHTML = null;
-      break;
-  }
-});
-
-//Tipo
-$("typeproductsId").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-typeproductsId").innerHTML =
-        "El tipo de producto es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-typeproductsId").innerHTML = null;
-      break;
-  }
-});
-
-//Categoria
-$("categoryId").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-categoryId").innerHTML =
-        "La categoria de producto es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-categoryId").innerHTML = null;
-      break;
-  }
-});
-
-//material
-$("materialsId").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-materialsId").innerHTML =
-        "El material de producto es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-materialsId").innerHTML = null;
-      break;
-  }
-});
-
-//Capacidad
-$("compatibilitieId").addEventListener("blur", function () {
-  switch (true) {
-    case !this.value:
-      this.classList.add("is-invalid");
-      $("error-compatibilitieId").innerHTML =
-        "La capacidad de producto es obligatorio";
-      break;
-    default:
-      this.classList.remove("is-invalid");
-      $("error-compatibilitieId").innerHTML = null;
-      break;
-  }
+  !error ? this.submit() : ($("msg-error").hidden = false);
 });
